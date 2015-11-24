07d3764aa38d78dce5ef12b38f8fd31c5b5c8d7d
Massimiliano Marcon <me@marcon.me>
2015-11-24 16:30:50 +0100

    Adds support for send command.

--- index.js
+++ index.js
@@ -4,7 +4,28 @@ var argv = require('minimist')(process.argv.slice(2)),
     path = require('path'),
     logger = require('./lib/logger');
 
-function help(){}
+var defaults = {
+    e: 'http://localhost:3000'
+};
+
+function header(){
+    logger.line();
+    logger.title('Github Webhook Tester');
+    logger.line();
+}
+
+function footer(){
+    logger.line();
+}
+
+function help(){
+    logger.title('Commands');
+    logger.command('list', 'lists all the available events');
+    logger.command('send <command>', 'sends event to webhook');
+    logger.line();
+    logger.title('Options');
+    logger.command('-e', 'set the endpoint, defaults to ' + defaults.e);
+}
 
 function list(){
     var events = path.join(__dirname, 'events');
@@ -20,10 +41,30 @@ function list(){
     });
 }
 
-switch(argv._[0]){
-    case 'list':
-        list();
-        break;
-    default:
-        help();
+function send(endpoint, event){
+    if(!event) {
+        return help();
+    }
+    logger.info('Sending %s event to %s', event, endpoint);
 }
+
+var availableCommands = {
+    help: help,
+    list: list,
+    send: send
+};
+
+var command = argv._.shift(),
+    arguments = argv._;
+
+arguments.unshift(argv.e || defaults.e);
+
+header();
+
+if(availableCommands[command]) {
+    availableCommands[command].apply(null, arguments);
+    footer();
+    return;
+}
+help();
+footer();
